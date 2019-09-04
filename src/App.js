07608dd1e4af26b9f63f228ebace54aa6212728a
Feather Knee <fknee@nvidia.com>
2019-09-04 16:38:50 -0700

    all styling done now functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FiFeather } from 'react-icons/fi'
 import { SearchInput } from './SearchInput'
 
 function App() {
 	return (
 		<div className='container'>
-      <nav>Welcome to SearchVille</nav>
+      <nav>Welcome to SearchVille <FiFeather /></nav>
       <div className='content'>
         <SearchInput label='search' />
       </div>
--- src/SearchInput/SearchInput.js
+++ src/SearchInput/SearchInput.js
@@ -1,32 +1,45 @@
 import React, { useState } from 'react'
-import { FiPlusSquare, FiXSquare } from 'react-icons/fi'
+import { FiPlusSquare, FiXSquare, FiX, FiFilter } from 'react-icons/fi'
 import './searchInput.css'
 
 const labels = [
 	{ name: 'Name', value: 'cuda' },
 	{ name: 'Latest Tag', value: '1.0' },
-	{ name: 'Size', value: [ 'large', 'medium', 'small' ] }
+  { name: 'Size', value: [ 'large', 'medium', 'small' ] },
+  { name: 'Module', value: 'cuda' },
+	{ name: 'Library', value: '1.0' },
+	{ name: 'Spec', value: [ 'large', 'medium', 'small' ] }
 ]
 
 export const SearchInput = ({ label }) => {
 	const [ labelsOpen, toggleLabels ] = useState(false)
 	return (
     <div className='search-input-component'>
+      <div className='filter-icon-container'>
+        <FiFilter className='filter-icon' />
+      </div>
       <div className='search-input-container'>
-        <label for='search-input'>{label}</label>
+        <label htmlFor='search-input'>{label}</label>
         <input id='search-input' type='search' className='search-input' />
         {labelsOpen ? (
           <div>
             <FiXSquare className='feather-icon' onClick={() => toggleLabels(!labelsOpen)} />
             <div className='search-input-label-container'>
-              {labels.map((label) => <span onClick={() => console.log('hi')}>{label.name}</span>)}
+              {labels.map((label, i) => <span key={i} onClick={() => console.log('hi')}>{label.name}</span>)}
             </div>
           </div>
         ) : (
+          <div>
           <FiPlusSquare className='feather-icon' onClick={() => toggleLabels(!labelsOpen)} />
+          </div>
         )}
       </div>
-      <button type='submit'>submit</button>
+      <div className='search-close-container'>
+          <FiX className='close-x' />
+      </div>
+      <div className='search-button-container'>
+        <button type='submit'>submit</button>
+      </div>
     </div>
 	)
 }
